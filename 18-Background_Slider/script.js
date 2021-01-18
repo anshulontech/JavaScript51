e1e3a080f796f28e9423f36d0b0a9e59b751bbbc
anshul7sh <anshul7sh@gmail.com>
2021-01-18 22:10:39 +0530

    Project Updated

---
+++ 18-Background_Slider/script.js
@@ -0,0 +1,40 @@
+const body = document.body;
+const slides = document.querySelectorAll(`.slide`);
+const leftBtn = document.getElementById(`left`);
+const rightBtn = document.getElementById(`right`);
+
+let activeSlide = 0;
+
+setBgToBody();
+
+rightBtn.addEventListener(`click`, () => {
+    activeSlide++;
+
+    if (activeSlide >= slides.length)
+        activeSlide = 0;
+
+    setBgToBody();
+    setActiveSlide();
+})
+
+leftBtn.addEventListener(`click`, () => {
+    activeSlide--;
+
+    if (activeSlide < 0)
+        activeSlide = slides.length - 1;
+
+    setBgToBody();
+    setActiveSlide();
+})
+
+function setBgToBody() {
+    body.style.backgroundImage = slides[activeSlide].style.backgroundImage
+}
+
+function setActiveSlide() {
+    slides.forEach((slide) => {
+        slide.classList.remove(`active`);
+    })
+
+    slides[activeSlide].classList.add(`active`);
+}
